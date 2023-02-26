de3985c975d9e94be06f09dfe3226e54a2ff7987
OG-Matcha <matchaskill@gmail.com>
2023-02-26 21:12:19 +0800

    Upload Input().py

---
+++ L6 Input()/6, Input().py
@@ -0,0 +1,12 @@
+# Input = used to take user input. By default, it returns the user input in form of a string.
+
+score = input("Please enter a number: ")
+print("Matcha's score is: " + score)
+print(int(score) + 10)
+print(int(score) - 10)
+print(int(score) * 10)
+
+age = int(input("Please enter your age: "))
+print(age + 10)
+print(age - 10)
+print(age * 10)
